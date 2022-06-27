62c8377f12fb6d15a6186a6781d83204c5445600
Brian McClain <brianmmcclain@gmail.com>
2022-06-27 14:14:27 -0400

    Function yaml.safeLoad is removed in js-yaml 4. Use yaml.load instead

--- workshop/config.js
+++ workshop/config.js
@@ -6,7 +6,7 @@ function initialize(workshop) {
     workshop.load_workshop();
 
     let fileContents = fs.readFileSync('/home/eduk8s/.kube/config');
-    let data = yaml.safeLoad(fileContents);
+    let data = yaml.load(fileContents);
 
     workshop.data_variable('user_token', data.users[0].user.token);
 }
